43881cc73bd1e0620e8225784b99dd11be8629f9
Jan Olderdissen <jan@olderdissen.com>
2020-12-12 14:49:02 +0100

    Got zexall to compile correctly with zasm. Also hacked up a wrapper for vivado testing.

--- zextest/build.sh
+++ zextest/build.sh
@@ -1,5 +1,11 @@
 !#/bin/sh -xe
 
 ~/zasm/zasm -buw zexdoc.z80
-od -A x -t x1z zexdoc.rom > rom.hex
-od -A x -t x1z zexdoc.com > com.hex
+od -A x -t x1z zexdoc.rom > zexdoc.rom.hex
+od -A x -t x1z zexdoc.com > zexdoc.com.hex
+
+~/zasm/zasm -buw zexall.z80
+od -A x -t x1z zexall.rom > zexall.rom.hex
+od -A x -t x1z zexall.com > zexall.com.hex
+
+~/zasm/zasm -buw zexwrap.z80
