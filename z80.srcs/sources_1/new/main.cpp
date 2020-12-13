eb3f9059e36689c2696b4c9644997b3d1839c809
Jan Olderdissen <jan@olderdissen.com>
2020-12-13 16:09:33 +0100

    Verilator integration

---
+++ z80.srcs/sources_1/new/main.cpp
@@ -0,0 +1,42 @@
+#include <verilated.h>
+#include "Vz80.h"
+
+vluint64_t main_time = 0;
+
+int main(int argc, char** argv)
+{
+  Verilated::commandArgs(argc, argv);
+  Vz80 * top = new Vz80;
+  top->reset_external = 1;
+  top->clk = 0;
+  while (!Verilated::gotFinish()) {
+    // printf("%lld %04x\n", main_time, top->z80__DOT__IP);
+    if (top->bdos_trap) {
+      int cmd = top->z80__DOT__registers__DOT__BC & 0xff;
+      int de = top->z80__DOT__registers__DOT__DE;
+      // printf("  trap %02x %04x\n", cmd, de);
+        switch (cmd) {
+	case 9:
+          while (top->z80__DOT__sys_ram__DOT__mem[de] != '$') {
+            printf("%c", top->z80__DOT__sys_ram__DOT__mem[de]);
+	    de++;
+          }
+        }
+    }
+    if (main_time >= 25) {
+      top->clk = !top->clk;
+    }
+    if (main_time == 100) {
+      // load rom here
+      top->reset_external = 0;
+    }
+    if (main_time >= 100) {
+      if (top->halt) 
+        break;
+    }
+    top->eval();
+    main_time += 25;
+  }
+  top->final();
+  delete top;
+}
--- z80.srcs/sources_1/new/make.sh
+++ z80.srcs/sources_1/new/make.sh
@@ -2,3 +2,4 @@
 ~/zasm/zasm -buw --ixcbr2 testbench.asm
 python3 tohex.py
 python3 make_ucode.py
+verilator --cc -Wno-WIDTH z80.v alu16.v alu_blk.v decode_cb_rom.v decode_xybits_rom.v registers.v sys_io.v ucode.v alu8.v decode1_rom.v decode_ed_rom.v decode_xy_rom.v sync_reset.v sys_ram.v --exe --build main.cpp
