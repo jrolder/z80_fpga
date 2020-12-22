22727f4ca198acfdf5d05115dd515284094e7711
Jan Olderdissen <jan@olderdissen.com>
2020-12-22 18:34:02 +0100

    zexdoc/all source and build cleanup

--- zextest/build.sh
+++ zextest/build.sh
@@ -1,12 +1,12 @@
-!#/bin/sh -xe
+#!/bin/sh -xe
 
-~/zasm/zasm -buw zexdoc.z80
+zasm -buw zexdoc.z80
 od -A x -t x1z zexdoc.rom > zexdoc.rom.hex
 od -A x -t x1z zexdoc.com > zexdoc.com.hex
 
-~/zasm/zasm -buw zexall.z80
+zasm -buw zexall.z80
 od -A x -t x1z zexall.rom > zexall.rom.hex
 od -A x -t x1z zexall.com > zexall.com.hex
 
-~/zasm/zasm -buw zexwrap.z80
+zasm -buw zexwrap.z80
 od -t x1 -A n -v zexwrap.rom > zexwrap.mem
